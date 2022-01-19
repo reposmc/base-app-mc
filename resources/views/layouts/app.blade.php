31bd36ee080da93854fc1e0e343af53f05667791
William <wpineda@cultura.gob.sv>
2022-01-19 08:49:10 -0600

    formularios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('img/Favicon.png') }}">
 </head>
 <body>
     <div id="app">
