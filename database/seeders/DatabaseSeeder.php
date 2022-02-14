19092cded00d8d38a102bbab77dc09166d536213
William <wpineda@cultura.gob.sv>
2022-02-14 15:27:56 -0600

    Migraciones

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             DepartmentSeeder::class,
+            MunicipalitiesSeeder::class,
             UserSeeder::class,
         ]);
     }
