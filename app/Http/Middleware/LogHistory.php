25c9f59c38001e953609ebeaab2a0b425fac7a89
Leonel Lopez <leonellopez647@gmail.com>
2022-03-22 08:21:58 -0600

    Log

---
+++ app/Http/Middleware/LogHistory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\Log;
+
+class LogHistory
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        Log::create([
+            'email' => auth()->user()->email,
+            'route' => url()->current(),
+            'type' => $request->method(),
+        ]);
+
+        return $next($request);
+    }
+}
---
+++ app/Models/Log.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Log extends Model
+{
+    use HasFactory;
+
+    protected $table = 'logs';
+
+    public $incrementing = false;
+
+
+    protected $fillable = [
+        'id',
+        'email',
+        'route',
+        'type',
+    ];
+
+    public $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public $timestamps = true;
+}
