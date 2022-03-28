12c663c61b44d276ca08687139446bb2355c828f
leolopezMC <92056647+leolopezMC@users.noreply.github.com>
2022-03-28 15:26:09 -0600

    Update EncryptController.php

--- app/Http/Controllers/EncryptController.php
+++ app/Http/Controllers/EncryptController.php
@@ -9,51 +9,76 @@ class EncryptController extends Controller
 {
     public static function encryptArray($array, array $keys)
     {
+        $data = [];
+
         foreach ($array as $model) {
             foreach ($keys as $value) {
-                $model[$value] = Crypt::encrypt($model[$value]);
+                $model->{$value} = Crypt::encrypt(strval($model[$value]));
             }
+            array_push($data, $model->getAttributes());
         }
 
-        return $array;
+        return $data;
     }
 
     public static function decryptArray($array, array $keys)
     {
+        $data = [];
+
         foreach ($array as $model) {
             foreach ($keys as $value) {
-                $model[$value] = Crypt::decrypt($model[$value]);
+                $model->{$value} = Crypt::decrypt($model[$value]);
             }
+            array_push($data, $model->getAttributes());
         }
 
-        return $array;
+        return $data;
     }
 
     public static function encryptModel($model, $key)
     {
-        $model[$key] = Crypt::encrypt($model[$key]);
+        $data = strval($model[$value]);
+        $model[$key] = Crypt::encrypt($data);
 
         return $model;
     }
 
     public static function decryptModel($model, $key)
     {
-        $model[$key] = Crypt::decrypt($model[$key]);
+        $model[$key] = intval(Crypt::decrypt($model[$key]));
 
         return $model;
     }
 
     public static function encryptValue($value)
     {
-        $value = Crypt::encrypt($value);
-
+        $data = strval($value);
+        $value = Crypt::encrypt($data);
         return $value;
     }
 
     public static function decryptValue($value)
     {
-        $value = Crypt::decrypt($value);
-
+        $value = intval(Crypt::decrypt($value));
         return $value;
     }
+
+    public static function encryptArrayValues($array, array $keys)
+    {
+        foreach ($keys as $value) {
+            $array[$value] = Crypt::encrypt($array[$value]);
+        }
+
+        return $array;
+    }
+
+    public static function decryptArrayValues($array, array $keys)
+    {
+        foreach ($keys as $value) {
+            $array[$value] = Crypt::decrypt($array[$value]);
+        }
+
+        return $array;
+    }
 }
+
