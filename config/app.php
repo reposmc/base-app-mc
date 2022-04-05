bb687790630df8f21d7537e80572e0ba22fc3b0e
Leonel Lopez <leonellopez647@gmail.com>
2022-04-05 08:02:41 -0600

    Backup library

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         Spatie\Permission\PermissionServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Leolopez\Backup\BackupServiceProvider::class
     ],
 
     /*
@@ -234,6 +235,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'Backup' => \Leolopez\Backup\Facades\Backup::class,
     ],
 
 ];
