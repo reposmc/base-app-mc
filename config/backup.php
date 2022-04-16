1fd8ec722e6ca7d949e258af6d41b42bf357911d
Leonel Lopez <leonellopz647@gmail.com>
2022-04-16 17:40:12 -0600

    Configuración backup

---
+++ config/backup.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'tenant_id' => env('TENANT_ID'),
+    'client_id' => env('CLIENT_ID'),
+    'client_secret' => env('CLIENT_SECRET'),
+    'username' => env('USERNAME'),
+    'password' => env('PASSWORD'),
+];
